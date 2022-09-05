f3d9e07b5c111e73a5b53c97019140b7d959bfaa
wadeen <meister.vvdo@gmail.com>
2022-09-05 13:31:24 +0900

    add: トップとLayoutコンポーネント作成

---
+++ __test__/Home.test.tsx
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Home from '../pages/index'
+
+it('Should render hello text', () => {
+  render(<Home />)
+  // screen.debug()
+  expect(screen.getByText('Hello Nextjs')).toBeInTheDocument()
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
+import Layout from '../components/Layout'
 const Home: React.FC = () => {
   return (
-    <div className="flex justify-center items-center flex-col min-h-screen font-mono">
-      Hello Nextjs
-    </div>
+    <Layout title="Home">
+      <p className="text-4xl">Welcome to Nextjs</p>
+    </Layout>
   )
 }
 export default Home

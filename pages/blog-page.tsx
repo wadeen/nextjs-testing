bf4e17ab974d7fa31c96343d3ff282c7b787bb6c
wadeen <meister.vvdo@gmail.com>
2022-09-05 16:47:34 +0900

    add: ブログ記事の取得(jsonplacehoder)

--- pages/blog-page.tsx
+++ pages/blog-page.tsx
@@ -1,6 +1,6 @@
 import Layout from '../components/Layout'
-// import { getAllPostsData } from '../lib/fetch'
-// import Post from '../components/Post'
+import { getAllPostsData } from '../lib/fetch'
+import Post from '../components/Post'
 import { GetStaticProps } from 'next'
 import { POST } from '../types/Types'
 
@@ -12,15 +12,15 @@ const BlogPage: React.FC<STATICPROPS> = ({ posts }) => {
   return (
     <Layout title="Blog">
       <p className="text-4xl mb-10">blog page</p>
-      {/* <ul>{posts && posts.map((post) => <Post key={post.id} {...post} />)}</ul> */}
+      <ul>{posts && posts.map((post) => <Post key={post.id} {...post} />)}</ul>
     </Layout>
   )
 }
 export default BlogPage
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const posts = await getAllPostsData()
-//   return {
-//     props: { posts },
-//   }
-// }
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getAllPostsData()
+  return {
+    props: { posts },
+  }
+}
